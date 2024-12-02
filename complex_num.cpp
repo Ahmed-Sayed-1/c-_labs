7657bbadaaf2eadc066b9bc2604b9afe674b2a45
Ahmed-Sayed-1 <ahmed2853519521@gmail.com>
2024-12-02 21:22:14 +0200

    first c++ lab

--- complex_num.cpp
+++ complex_num.cpp
@@ -59,25 +59,7 @@ public:
             cout<<img<<"i"<<endl;
         }
 };
-    void swap1(int *x,int *y)
-    {
-        *x=*x+*y;
-        *y=*x-*y;
-        *x=*x-*y;
-    }
-    void swap2(int &x,int &y)
-    {
-        x=x+y;
-        y=x-y;
-        x=x-y;
-    }
-    void swap3(int x,int y)
-    {
-        x=x+y;
-        y=x-y;
-        x=x-y;
-    }
-
+   
 
 int main()
 {
